15bdb9848bcd89c2c7ef362b47f9cad736eea6ce
DanielSilvaMonteiro <monteiro.dan@outlook.com>
2022-03-22 23:36:29 -0300

    first commit

---
+++ src/Mainquadrilatero.java
@@ -0,0 +1,8 @@
+public class Mainquadrilatero {
+    public static void main(String[] args) {
+        System.out.println("Exercicio quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5,5);
+        Quadrilatero.area(7,8,9);
+    }
+}
